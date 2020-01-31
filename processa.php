590454ae77bad5a637b5a3b4aa172223b329b5a0
J. Paulo de Almeida <j.paulo.de.almeida@avanade.com>
2020-01-31 18:24:29 -0300

    ajustes

--- processa.php
+++ processa.php
@@ -44,15 +44,15 @@
         }
 
         echo 
-        '<div class="container-fluid modal-dialog modal-dialog-centered">
+        '<div class="container-fluid modal-dialog-centered">
             <div class="alert alert-success" role="alert">
                 <h4 class="alert-heading">Acabamos!</h4>
                 <p>Agora você já pode abrir seu spotify e curtir a playlist que preparei para você com todas as suas musicas!</p>
                 <hr>
                 <p class="mb-0">Se você curtiu, me segue no insta para dar aquela moral!! <a href="https://www.instagram.com/eu.jpe?r=nametag" target="_blank">@eu.jpe</a></p>
-            </div>
-            <div class="text-center">
+                <div class="text-center">
                 <a href="https://minhaplaylist.azurewebsites.net/" class="btn btn-success">Ir para minhas Playlists</a>
+            </div>
             </div>
         </div>';
 ?>
