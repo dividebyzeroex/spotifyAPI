5a1d423c9f5806861f70a00cfc6b7800873a680f
J. Paulo de Almeida <j.paulo.de.almeida@avanade.com>
2020-01-31 16:01:37 -0300

    ajustes

--- review.php
+++ review.php
@@ -7,7 +7,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </head>
-    <body style="background: url('background-cover.jpg') no-repeat fixed center;">
+    <body>
       <?php
           require 'vendor/autoload.php';
           session_start();
@@ -31,6 +31,7 @@
             }
       
         } catch (\Throwable $th) {
+            echo $th;
             $session->refreshAccessToken($_SESSION["refreshToken"]);
             $accessToken = $session->getAccessToken();
         }
